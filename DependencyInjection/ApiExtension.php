2630f722f9e2ed08d6dfaeefdb38a5b41a4a6519
polonair <fixnim@gmail.com>
2017-01-23 11:20:30 +0400

    Исправлены ошибки
    Добавлены заглушки для интерфейсов администратора, менеджера и
    партнера, исправлены ошибки.
    
    Изменено:
      - Controller/AdminController.php, ManagerController.php,
        PartnerController.php - добавлены заглушки для интерфейсов
        администратора, менеджера и партнера;
      - Controller/MasterController.php - поправлен платежный интерфейс;
      - DependencyInjection/ApiExtension.php - исправлена ошибка с именем
        класса;
      - Resources/config/api.xml - удален пустой селектор;
      - Resources/views/admin.html.twig, manager.html.twig,
        master.html.twig, partner.html.twig - поправлены ссылки на
        фронтенд.

--- DependencyInjection/ApiExtension.php
+++ DependencyInjection/ApiExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class MasterExtension extends Extension
+class ApiExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
